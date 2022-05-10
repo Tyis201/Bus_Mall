5927687722a99d7fd05de221798f6375cd238421
tyisyustat24 <tyis.yustat24@icloud.com>
2022-05-10 09:11:24 -0700

    everything

--- script.js
+++ script.js
@@ -2,20 +2,13 @@
 
 console.log('hello world');
 
-const picContainerElement = document.getElementById
-('centerContent');
-const firstpicElement = document.getElementById
-('pic1');
-const secondpicElement = document.getElementById
-('pic2');
-const thirdpicElement= document.getElementById
-('pic3')
-const firstPicTitle = document.getElementById 
-('img1title');
-const secondPicTitle = document.getElementById
-('img2title');
-const thirdPicTitle = document.getElementById
-('img3title')
+const picContainerElement = document.getElementById('centerContent');
+const firstpicElement = document.getElementById('pic1');
+const secondpicElement = document.getElementById('pic2');
+const thirdpicElement= document.getElementById('pic3')
+const firstPicTitle = document.getElementById('img1title');
+const secondPicTitle = document.getElementById('img2title');
+const thirdPicTitle = document.getElementById('img3title');
 
 let count = 0
 
@@ -35,7 +28,7 @@ Image.allImages = [];
 Image.prototype.renderImage = function(img, h2){
     img.src = this.imgPath;
     h2.textContent = this.name;
-    this.views++;
+    this.views ++;
 }
 
 Image.allImages.push(new Image('Bag', './images/bag.jpg'));
@@ -78,21 +71,21 @@ Image.allImages.push(new Image('Wine-Glass', './images/wine-glass.jpg'));
 
 function changethreeimages() {
 const cantUse = [pic1, pic2, pic3];
-if(cantUse.includes(pic1)){
+while(cantUse.includes(pic1)){
     let picOne = Math.floor(Math.random() * Image.allImages.length);
     pic1 =Image.allImages[picOne];
 }
 cantUse.push[[pic1]]
-if(cantUse.includes(pic2)){
+while(cantUse.includes(pic2)){
 let picTwo = Math.floor(Math.random() * Image.allImages.length);
-pic2 = Image.allImages[picTwo]
+pic2 = Image.allImages[picTwo];
 }
-cantUse.push[[pic2]]
-if(cantUse.includes(pic3)){
+cantUse.push[pic2];
+while(cantUse.includes(pic3)){
     let picThree = Math.floor(Math.random() * Image.allImages.length);
     pic3 = Image.allImages[picThree]
     }
-    cantUse.push[[pic3]]
+    cantUse.push[pic3];
 }
 
 function renderImage() {
@@ -115,39 +108,114 @@ if(storedImage){
     let newImage = JSON.parse(storedImage);
     for(let image of newImage){
         let myNewImage = new Image(image.name, image.imgPath, image.clicks, image.view);
+        Image.allImages.push(myNewImage);
     }
 }    
 }
 
-function clickImage(e) {
-    let imageClicked = e.target.id;
-    if(imageClicked === 'pic1' || imageClicked === 'pic2' || imageClicked === 'pic3'){
-        count++;
+function makeChart(){
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    let imageNames = [];
+    let imageClicks = [];
+    let imageViews = [];
+
+    for(let image of Image.allImages){
+      imageNames.push(image.name);
+      imageClicks.push(image.clicks);
+      imageViews.push(image.views);
     }
-    if(imageClicked === 'pic1'){
-        pic1.clicks++;
-    }
-    if(imageClicked === 'pic2'){
-        pic2.clicks++;
-    }
-    if(imageClicked === 'pic3'){
-        pic3.clicks++;
-    }
-    if (count ===5){
-        for(let img of Image.allImages){
-            console.log(img.name + "Was Clicked on" + img.clicks + " Was viewed " + img.views)
+
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: imageNames,
+            datasets: [{
+                label: '# of Votes',
+                data: imageClicks,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },
+            {
+              label: '# of Views',
+              data: imageViews,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-    }
+    });
+  }
+  
+  function removeImages(){
+    document.getElementById('imgContainer1').style.display = 'none';
+    document.getElementById('imgContainer2').style.display = 'none';
+    document.getElementById('imgContainer3').style.display = 'none';
+  }
 
 
- getImagesFromStorage();
- changethreeimages();
-renderImage();
-    }
+    function clickImage(e) {
+        let imageClicked = e.target.id;
+        if(imageClicked === 'pic1' || imageClicked === 'pic2' || imageClicked === 'pic3'){
+            count++;
+        }
+        if(imageClicked === 'pic1'){
+            pic1.clicks++;
+        }
+        if(imageClicked === 'pic2'){
+            pic2.clicks++;
+        }
+        if(imageClicked === 'pic3'){
+            pic3.clicks++;
+        }
+        changethreeimages();
+        renderImage();
+
+        if (count === 5){
+            removeImages();
+            makeChart();
+            }
+         putImagesInStorage();
+        }
 
  picContainerElement.addEventListener('click', clickImage );
 
  getImagesFromStorage();
  changethreeimages();
 renderImage();
-
